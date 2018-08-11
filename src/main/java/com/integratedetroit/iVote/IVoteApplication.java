afbccd13c98d2ec3a5dfd3f6d797bf9e51ea7ee1
Chemley <canaowens@gmail.com>
2018-08-11 10:46:33 -0400

    initial commit

---
+++ src/main/java/com/integratedetroit/iVote/IVoteApplication.java
@@ -0,0 +1,12 @@
+package com.integratedetroit.iVote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IVoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IVoteApplication.class, args);
+	}
+}

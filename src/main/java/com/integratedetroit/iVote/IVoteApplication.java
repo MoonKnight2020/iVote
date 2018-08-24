440ae945a4d0fa94771f946eb45ab684eadd29e9
Samah Majadla <samah@Samahs-MacBook-Pro.local>
2018-08-24 18:14:27 -0400

    application is now pointing to postgresql database hosted on ec2

--- src/main/java/com/integratedetroit/iVote/IVoteApplication.java
+++ src/main/java/com/integratedetroit/iVote/IVoteApplication.java
@@ -3,8 +3,10 @@ package com.integratedetroit.iVote;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class IVoteApplication {
 
 	public static void main(String[] args) {
